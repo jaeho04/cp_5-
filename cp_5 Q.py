5fbd62a040e99ce619762204fd5b8831e71650dc
임재호 <wogh3245@naver.com>
2024-04-05 15:39:18 +0900

    cp_5주차 Q
    학점 환산 프로그램

---
+++ cp_5 Q.py
@@ -0,0 +1,11 @@
+score = input("점수를 입력하세요")
+num_score = int(score)
+
+if num_score >= 71 :
+        print("A")
+elif num_score >= 41 :
+        print("B")
+elif num_score >= 11 :
+        print("C")
+else :
+        print("D")
